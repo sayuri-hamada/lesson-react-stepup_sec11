ab60c525929c11365173d2fc77343a71d9c67677
Sayuri Hamada <110365626+sayuri-hamada@users.noreply.github.com>
2022-08-22 18:26:51 +0900

    カスタムフックを使わないデータ取得例

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,54 @@
+import axios from "axios";
+import { useState } from "react";
+import { UserCard } from "./components/UserCard";
 import "./styles.css";
+import { User } from "./types/api/User";
+import { UserProfile } from "./types/UserProfile";
 
 export default function App() {
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const onClickFetchUser = () => {
+    //エラーとローディングの初期値を設定する
+    setLoading(true);
+    setError(false);
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}（${user.username}）`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`
+        }));
+        setUserProfiles(data);
+      })
+      .catch(() => {
+        //エラーがあった場合はエラーのstateをtrueに
+        setError(true);
+      })
+      .finally(() => {
+        //処理が終了したらloadingのstateをfalseに
+        setLoading(false);
+      });
+  };
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={onClickFetchUser}>データ取得</button>
+      <br />
+      {error ? (
+        <p style={{ color: "red" }}>データの取得に失敗しました</p>
+      ) : loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {userProfiles.map((user) => (
+            <UserCard user={user} key={user.id} />
+          ))}
+        </>
+      )}
     </div>
   );
 }
