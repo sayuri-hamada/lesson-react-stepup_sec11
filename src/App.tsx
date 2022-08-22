4be73e57bd52fd858f7ebcb7ef124ba6bb554cff
Sayuri Hamada <110365626+sayuri-hamada@users.noreply.github.com>
2022-08-22 18:37:56 +0900

    カスタムフックを呼び出す

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,11 @@
-import axios from "axios";
-import { useState } from "react";
 import { UserCard } from "./components/UserCard";
 import "./styles.css";
-import { User } from "./types/api/User";
-import { UserProfile } from "./types/UserProfile";
+import { useAllUsers } from "./hooks/useAllUsers";
 
 export default function App() {
-  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-
-  const onClickFetchUser = () => {
-    //エラーとローディングの初期値を設定する
-    setLoading(true);
-    setError(false);
-    axios
-      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => {
-        const data = res.data.map((user) => ({
-          id: user.id,
-          name: `${user.name}（${user.username}）`,
-          email: user.email,
-          address: `${user.address.city}${user.address.suite}${user.address.street}`
-        }));
-        setUserProfiles(data);
-      })
-      .catch(() => {
-        //エラーがあった場合はエラーのstateをtrueに
-        setError(true);
-      })
-      .finally(() => {
-        //処理が終了したらloadingのstateをfalseに
-        setLoading(false);
-      });
-  };
+  // useAllUsersから受け取った関数やstateを利用できるようにする
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
+  const onClickFetchUser = () => getUsers();
   return (
     <div className="App">
       <button onClick={onClickFetchUser}>データ取得</button>
